998627416e4bb5d69d50a69c0ef83e5125d3461b
Tom Burrows <rc0027592@student.riversidecollege.ac.uk>
2019-06-17 13:47:30 +0100

    Added comments to the code and updated label 2

--- Assignment 2/Form1.cs
+++ Assignment 2/Form1.cs
@@ -19,6 +19,7 @@ namespace Assignment_2
 
         private void button1_Click(object sender, EventArgs e)
         {
+            // this is all connected to the button for the series circuit to calculate the total resistance
             try
             {
                 double r1 = double.Parse(textBox1.Text);
@@ -28,16 +29,19 @@ namespace Assignment_2
                 double r5 = double.Parse(textBox8.Text);
                 double answer = r1 + r2 + r3 + r4 + r5;
                 label1.Text = "Total resistance = " + answer.ToString();
+                // this is how the resistor values are calculated tot get the total resistance of the series
             }
             catch
             {
-                label1.Text = "Total resistance = ";
+                label1.Text = "Incorrect  ";
+                // this is the catch statement is the input on the resistance is incorrect or invalid it will display this
             }
 
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            // this is all connected to the button for the parallel circuit to calculate the total resistance
             try
             {
                 double r1 = double.Parse(textBox4.Text);
@@ -47,10 +51,12 @@ namespace Assignment_2
                 double r5 = double.Parse(textBox10.Text);
                 double answer = 1.0 / (1.0 / r1 + 1.0 / r2 + 1.0 / r3 + 1.0 / r4 + 1.0 / r5);
                 label2.Text = "Total resistance = " + answer.ToString();
+                // this is how the resistor values are calculated tot get the total resistance of the series
             }
             catch
             {
-                label2.Text = "Total resistance = ";
+                label2.Text = "Incorrect  ";
+                // this is the catch statement is the input on the resistance is incorrect or invalid it will display this
             }
 
 
